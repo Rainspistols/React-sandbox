3bc53eed5393a883bf2ca85c7d94359eef6256ec
Rainspistols <ffly@mail.ru>
2020-03-07 15:21:33 +0100

    reducer with params(payloads)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const reducer = (state = 0, action) => {
       return state + 1;
     case 'DEC':
       return state - 1;
+    case 'RND':
+      return state + action.payload;
 
     default:
       return state;
@@ -19,6 +21,10 @@ document.getElementById('dec').addEventListener('click', () => {
 });
 document.getElementById('inc').addEventListener('click', () => {
   store.dispatch({ type: 'INC' });
+});
+document.getElementById('rnd').addEventListener('click', () => {
+  const payload = Math.floor(Math.random() * 10);
+  store.dispatch({ type: 'RND', payload });
 });
 
 const update = () => {
